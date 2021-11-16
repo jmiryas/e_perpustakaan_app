43818c9521be644b72c94a82f2ab920282c599a3
Rizky Ramadhan <jmiryas@gmail.com>
2021-11-16 20:59:53 +0700

    Add drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,10 +6,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Katalog"),
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Katalog"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("images/wallpaper.jpg"))),
+                child: Center(
+                  child: Text(
+                    "e-Perpustakaan",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text("Library"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Borrowing History"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Waitlist"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("About Us"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
